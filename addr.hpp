b4d0a8564b38b28afb82e79effd84aad50a82909
NemanjaKusic <nemanja.kusic99@gmail.com>
2025-03-27 11:43:13 +0100

    clock period (delay) of 6ns added

--- addr.hpp
+++ addr.hpp
@@ -7,7 +7,7 @@
 typedef tlm::tlm_base_protocol_types::tlm_payload_type pl_t;
 typedef tlm::tlm_base_protocol_types::tlm_phase_type ph_t;
 
-#define DELAY 10
+#define DELAY 6
 
 #define IMG_COLS 600
 #define IMG_ROWS 450
